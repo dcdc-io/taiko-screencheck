e57c274c3a4a1b11113b328d5ac326c62579dea3
ben@dcdc.io <ben@dcdc.io>
2020-02-13 11:20:53 +0000

    tests

---
+++ src/spec/ScreenCheck.spec.ts
@@ -0,0 +1,180 @@
+/* mock-fs is priority import */
+import mockfs from "mock-fs"
+import * as fse from "fs-extra"
+import * as assert from "assert"
+import { ScreenCheck, ScreenCheckResult, ScreenCheckResultType } from "../index"
+import path from "path"
+
+describe('ScreenCheck', () => {
+    const taiko = {
+        currentURL: () => "https://dcdc.io/events#latest"
+    }
+    // @ts-ignore
+    ScreenCheck.init(taiko)
+
+    describe('#setup()', async () => {
+        const expectedBaseDir = path.join(__dirname, "temp_safe_to_delete")
+
+        let fs = {}
+        // @ts-ignore
+        fs[path.join(__dirname, "temp_safe_to_delete")] = {}
+        mockfs(fs)
+
+        it('should set ScreenCheck.baseDir', async () => {
+            assert.notEqual(ScreenCheck.baseDir, expectedBaseDir)
+            await ScreenCheck.setup({baseDir: expectedBaseDir})
+            assert.equal(ScreenCheck.baseDir, expectedBaseDir)
+        })
+
+        it('should set ScreenCheck.runId', async () => {
+            await ScreenCheck.setup({baseDir: expectedBaseDir})
+            // assert.equal(ScreenCheck.runId.split("-").length, 2)
+
+        })
+    })
+
+    describe('#simplifyPath', async () => {
+        it('should remove all unsafe characters', async () => {
+            const badPath = "C:/windows/system32/image.png"
+            const expectedPath = "C-windows-system32-image-png"
+            const actualPath = ScreenCheck.simplifyPath(badPath)
+            assert.equal(actualPath, expectedPath)
+        })
+    })
+
+    describe('#generateFilename', async () => {
+        it('should generate a path without element searching', async () => {
+            const expectedFilename = "dcdc-io-events-latest"
+            // @ts-ignore
+            const actualFilename = await ScreenCheck.generateName(taiko)
+            assert.equal(actualFilename, expectedFilename)
+        })
+
+        it('should generate a path with element searching', async () => {
+            const expectedFilename = "dcdc-io-events-latest-407833-fullpage"
+            const textElement = { description: 'Element with text "hello"' }
+            // @ts-ignore
+            const actualFilename = await ScreenCheck.generateName(taiko, true, textElement)
+            assert.equal(actualFilename, expectedFilename)
+        })
+    })
+
+    describe("#screencheck", () => {
+        beforeEach(async () => {            
+            // @ts-ignore
+            await ScreenCheck.init(taiko)
+            await ScreenCheck.setup({baseDir:"", runId:"0001.auto"})
+            mockfs.restore()
+        })
+        it('should take a screenshot without arguments', async () => {
+            let hit = 0
+            // @ts-ignore
+            taiko.screenshot = async () =>  {
+                hit++
+                mockfs({ "0001.auto/dcdc-io-events-latest.png": "0123456789" })
+            }
+            await ScreenCheck.screencheck()
+            assert.equal(hit, 1)
+        })
+
+        it('should take a screenshot with arguments', async () => {
+            let hit = 0
+            let callargs:any[] = []
+            // @ts-ignore
+            taiko.screenshot = async function() {
+                // @ts-ignore 
+                callargs = arguments
+                mockfs({ "0001.auto/dcdc-io-events-latest-0cc175-92eb5f-fullpage.png": "0123456789" })
+             }
+            await ScreenCheck.screencheck({fullPage:true, encoding:"base64"}, {description:"a"}, {description:"b"})
+            assert.equal(
+                JSON.stringify(Array.from(callargs)),
+                JSON.stringify([{fullPage:true, encoding:"base64", path: ScreenCheck.getRunDir() + "/dcdc-io-events-latest-0cc175-92eb5f-fullpage.png"}, {description:"a"}, {description:"b"}])
+            )
+        })
+
+        it('should return NO_BASE_IMAGE result when there is no base image', async () => {
+            const fs = {}
+            // @ts-ignore
+            taiko.screenshot = async (options) => {
+                mockfs.restore()
+                // @ts-ignore
+                fs[options.path] = fse.readFileSync(path.join(__dirname, "image_1.png"))
+                mockfs(fs)
+            }
+            const result = await ScreenCheck.screencheck()
+            assert.equal(result.result, ScreenCheckResultType.NO_BASE_IMAGE)
+            assert.ok(result.data)
+            assert.equal(result.referenceData, undefined)
+        })
+
+        it('should return SAME when base and new image match', async () => {
+            mockfs.restore()
+            const fs = {
+                "reference/dcdc-io-events-latest.png": fse.readFileSync(path.join(__dirname, "image_1.png"))
+            }
+            mockfs(fs)
+            // @ts-ignore
+            taiko.screenshot = async (options) => {
+                mockfs.restore()
+                // @ts-ignore
+                fs[options.path] = fse.readFileSync(path.join(__dirname, "image_1.png"))
+                mockfs(fs)
+            }
+            const result = await ScreenCheck.screencheck()
+            assert.equal(result.result, ScreenCheckResultType.SAME)
+            assert.ok(result.data)
+            assert.equal(result.referenceData, undefined)
+        })
+
+        it('should return DIFFERENT when base and new image differ', async () => {
+            mockfs.restore()
+            const fs = {
+                "reference/dcdc-io-events-latest.png": fse.readFileSync(path.join(__dirname, "image_1.png"))
+            }
+            mockfs(fs)
+            // @ts-ignore
+            taiko.screenshot = async (options) => {
+                mockfs.restore()
+                // @ts-ignore
+                fs[options.path] = fse.readFileSync(path.join(__dirname, "image_2.png"))
+                mockfs(fs)
+            }
+            const result = await ScreenCheck.screencheck()
+            assert.equal(result.result, ScreenCheckResultType.DIFFERENT)
+            assert.ok(result.data)
+            assert.ok(result.referenceData)
+        })
+    })
+
+    describe('#compareImages', async () => {
+        it('should produce a diff image', async () => {
+            mockfs.restore()
+            const image1 = `${__dirname}/image_1.png`
+            const image2 = `${__dirname}/image_2.png`
+            const diff = await ScreenCheck.compareImages(image1, image2)
+            assert.equal(diff.missmatching, 719)            
+        })
+    })
+
+    describe('#getReferenceImagePath', async () => {
+        it('should return null when there is no previous image', async () => {
+            await ScreenCheck.setup({ baseDir:"/mock", runId: "0001" })   
+            const previousImage = await ScreenCheck.getReferenceImagePath("fake.png")
+            assert.equal(previousImage, undefined)
+        })
+
+        it('should return a path when a previous image exists', async () => {            
+            await ScreenCheck.setup({ baseDir:"/mock", runId: "0001" })   
+            mockfs({
+                '/mock/reference': {
+                    'fake.png': '0123456789'
+                }
+            })
+            const previousImage = await ScreenCheck.getReferenceImagePath("fake.png")
+            assert.ok(previousImage)
+            const expectedImage = "/mock/reference/fake.png"
+            assert.equal(path.normalize(previousImage!), path.normalize(expectedImage))
+        })
+    })
+})
